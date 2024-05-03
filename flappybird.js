c0dcb3eb55d6cc6039121a073ea34597358bef55
Bhavana-Uppalapati <ubhavana1012@gmail.com>
2024-05-03 19:47:31 +0530

    flappy bird

--- flappybird.js
+++ flappybird.js
@@ -1,44 +1,52 @@
-const space= document.getElementById("space");
-var result = document.getElementById("result_box");
+
+const hole= document.getElementById("space");
+var result = document.getElementById("result");
 var text = document.getElementById("text");
 var game  = document.getElementById("game");
-var bird = document.getElementById('bird')
-var block = document.getElementById("block")
-// let game_msg = document.getElementById('h1')
 var jumping = 0;
 var score = 0;
+var audio = new Audio();
+audio.src = "./abc.mp3"
 
 //animation 
 
-space.addEventListener("animationiteration", hole)
+hole.addEventListener("animationiteration", hol)
 
-function hole(){
+function hol(){
     var random = -((Math.random()*350) +150)
-   space.style.top = random + "px"
+   hole.style.top = random + "px"
    score++;
 }
+
 
 //fall and game over
  var fall = setInterval(function(){
      var birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
-      if(jumping == 0)
-      {bird.style.top = (birdTop + 2) + "px";}
+      if(jumping == 0){bird.style.top = (birdTop + 2) + "px";}
 
       var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
-      var spaceTop = parseInt(window.getComputedStyle(space).getPropertyValue("top"))
-      var hTop = (500 + spaceTop);
+      var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"))
+      var hTop = (500 + holeTop);
       if((birdTop > 450) || ((blockLeft<50)&& (blockLeft > -50) && ((birdTop < hTop) || (birdTop > hTop + 100 )))){
+        audio.pause()
           result.style.display = "block";
           text.innerText = `Score : ${score}`;
+          game.style.display = "none";
           score = 0;
-        clearInterval(fall)
-         
-}
-},10)
-//jump
+
+          
+      }
+
+     
+
+
+ },10)
+
+
+ //jump
 function jump(){
    jumping = 1;
-
+   audio.play()
    var birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
    if(birdTop > 6){bird.style.top = (birdTop - 60 ) + "px"}
 
